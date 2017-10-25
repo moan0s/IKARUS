00ba618ec418517fcca449a7d0396e5bb2d6bb81
Jobbel <jckieser@gmx.de>
2017-10-25 13:39:14 +0200

    Initial Commit

---
+++ src/main.java
@@ -0,0 +1,22 @@
+
+import lejos.hardware.Power;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.Button;
+
+
+public class main {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LCD.clear();
+
+		System.out.println("hi");
+
+		Button.waitForAnyPress();
+	}
+
+}
