6c2bbfdfb106b387da8714848c508fe37ceb1b54
Jobbel <jckieser@gmx.de>
2017-10-25 17:49:20 +0200

    first commit with contetn

--- src/main.java
+++ src/main.java
@@ -1,22 +1,64 @@
 
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+import lejos.utility.Timer;
 import lejos.hardware.Power;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.Button;
-
 
 public class main {
 
-	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		
-		LCD.clear();
+		
+		RegulatedMotor m1= new EV3LargeRegulatedMotor(MotorPort.A);
+		RegulatedMotor m2 = new EV3LargeRegulatedMotor(MotorPort.B);
+		
+		PID pid; 
+		
+		pid = new PID(0.25, 0.01, 0.4);
+		pid.setOutputLimits(10);
+		//miniPID.setMaxIOutput(2);
+		//miniPID.setOutputRampRate(3);
+		//miniPID.setOutputFilter(.3);
+		pid.setSetpointRange(40);
+		
+		double sensor = 0;
+		double target = 0;
 
-		System.out.println("hi");
-
+		
+		
+		gyro gy = new gyro();
+		
+		gy.init();
+		
+		System.out.println("Roboter aufrecht ausrichten und einen Knopf betätigen");
+		
 		Button.waitForAnyPress();
+		gy.resetGyro();
+
+		int button = 0;
+		
+		while(((button = Button.waitForAnyPress(1)) == 0)){
+			  //get some sort of sensor value
+			sensor = gy.getGyroAngle();
+			  //set some sort of target value
+			  double output=pid.getOutput(sensor,target);
+			  //do something with the output
+			  //Delay(10);
+			  System.out.println(output);
+			  m1.rotate((int)output*5);
+			  m2.rotate((int)output*5);
+			}
+		
+		
+		  while (((button = Button.waitForAnyPress(50)) == 0)) {
+				System.out.println("gyro: ");
+				System.out.println(gy.getGyroAngle());
+			  }
+		
 	}
 
 }
